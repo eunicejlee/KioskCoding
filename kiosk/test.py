3fa8c165cd78828fc34a60a1c20bf92cd251d096
eunicejlee <43863945+eunicejlee@users.noreply.github.com>
2020-06-02 12:10:28 -0700

    Delete test.py

--- kiosk/test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from customer import Customer
-from item import Item
-from inventory import Inventory
-from purchase import Purchase
-from paymenttype import Paymenttype
-from payment import Payment
-
-class CustomerTest (unittest.Testcase):
